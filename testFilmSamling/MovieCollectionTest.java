979f54a3592d657bf196951849e1be51f78c292b
lucasmodin <lumo0002@stud.kea.dk>
2024-04-11 19:47:13 +0200

    Made the saveMethod to only save if changes are made. Also the CSV file is loaded on program startup and when the user exits the program, changes are saved (if there are any)

--- testFilmSamling/MovieCollectionTest.java
+++ testFilmSamling/MovieCollectionTest.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MovieCollectionTest implements fileHandlerInteface{
+class MovieCollectionTest implements fileHandlerInteface {
 
     //mock file
     String filePath = "test.csv";
@@ -52,7 +52,7 @@ class MovieCollectionTest implements fileHandlerInteface{
 
         MovieCollection actualFilm = new MovieCollection();
 
-        actualFilm.tilFøjMovie("DieHard","Lord Hard",2004, true, 20, "Action");
+        actualFilm.tilFøjMovie("DieHard", "Lord Hard", 2004, true, 20, "Action");
 
         String expectedMovies = actualFilm.getMovietitles();
 
@@ -68,59 +68,65 @@ class MovieCollectionTest implements fileHandlerInteface{
     void getMovietitles() {
 
 
-            // Arrange -
-            // set det up.
+        // Arrange -
+        // set det up.
         ArrayList<Movie> movie = new ArrayList<>();
 
         MovieCollection film = new MovieCollection();
 
 
-
-        film.tilFøjMovie("DieHard","Lord Hard",2004, true, 20, "Action");
-        film.tilFøjMovie("Lovepotion","Jacob Clienton",1992, false, 32, "Drama");
-        film.tilFøjMovie("Kitler","Hitler",1992, false, 32, "Drama");
+        film.tilFøjMovie("DieHard", "Lord Hard", 2004, true, 20, "Action");
+        film.tilFøjMovie("Lovepotion", "Jacob Clienton", 1992, false, 32, "Drama");
+        film.tilFøjMovie("Kitler", "Hitler", 1992, false, 32, "Drama");
 
         // Act -
-            // lave en string med input.
+        // lave en string med input.
         ArrayList<Movie> movieActual = new ArrayList<>();
 
         MovieCollection actualFilm = new MovieCollection();
 
-        actualFilm.tilFøjMovie("DieHard","Lord Hard",2004, true, 20, "Action");
-        actualFilm.tilFøjMovie("Kitler","Hitler",1992, false, 32, "Drama"); // Man kan ikke bytte om på objecterne.
-        actualFilm.tilFøjMovie("Lovepotion","Jacob Clienton",1992, false, 32, "Drama");
+        actualFilm.tilFøjMovie("DieHard", "Lord Hard", 2004, true, 20, "Action");
+        actualFilm.tilFøjMovie("Kitler", "Hitler", 1992, false, 32, "Drama"); // Man kan ikke bytte om på objecterne.
+        actualFilm.tilFøjMovie("Lovepotion", "Jacob Clienton", 1992, false, 32, "Drama");
 
 
         // String currentMovies = film.getMovietitles();
 
-            // Assert
+        // Assert
 
-            //String wantedMovies = actualFilm.getMovietitles();
+        //String wantedMovies = actualFilm.getMovietitles();
 
-            //assertEquals(wantedMovies, currentMovies);
+        //assertEquals(wantedMovies, currentMovies);
 
     }
 
 
-
-
     @Test
-    void loadMovieCollection(){
+    void loadMovieCollection() {
 
         //Arrange
-
-
+        //Create movieCollection object
+        MovieCollection movieCollection = new MovieCollection();
 
         //Act
+        //call upon the save helper method since our own save method saves directly to our moviecollection, and we don't want that
+        //(see bottom of file for method)
+        saveMoviesToCsvFileHelper();
 
+        //Load the movie objects from the CSV file into the movieCollection arraylist
+        loadMovieCollection(movieCollection);
 
         //Assert
-
-
+        //check if the file exists
+        assert(file.exists());
+        //check if the movieCollection arraylist contains 3 objects (we added 3 to the file in the helper method)
+        assertEquals(3, movieCollection.getMovieCollection().size());
+        file.delete();
+        //delete the file after
     }
 
     @Test
-    void saveMovicCollection(){
+    void saveMovicCollection() {
 
 
         //Arrange
@@ -147,7 +153,7 @@ class MovieCollectionTest implements fileHandlerInteface{
         for (int i = 0; i < actualMovieCollection.size(); i++) {
             assertEquals(0, actualMovieCollection.get(i).compareTo(loadedMovieCollection.get(i)));
             //check for the contents of the 2 arraylist utilising the compareTo method in Movie class.
-            // The '0' is the expected result, as the compareTo method in Movie will return 0 if the contents is the same
+            // The '0' is the expected result, as the compareTo method in Movie will return 0 if the contents are the same
         }
         file.delete(); //remove the file after testing
 
@@ -155,7 +161,35 @@ class MovieCollectionTest implements fileHandlerInteface{
     }
 
     @Test
-    void dontDoubleSave(){
+    void dontDoubleSave() {
+
+        // Arrange
+        //Make movieCollection object and add movies to the movieCollection ArrayList
+        MovieCollection movieCollection = new MovieCollection();
+        movieCollection.tilFøjMovie("hans", "jensen", 1999, true, 92, "action");
+        movieCollection.tilFøjMovie("john", "hansen", 1989, true, 98, "kkkkkk");
+
+
+        //set the isChanged to false
+        boolean isChanged = false;
+
+
+        //Act
+        //When isChanged is set to true, it will create a new file called "test.csv" and save to this file
+        //If the isChanged is set to false, it will never create a file and it doesn't save. We are testing with false as the method shouldn't save if nothing is changed
+        saveMovieCollection(movieCollection, isChanged);
+
+
+        //Assert
+        //We assert if the file exists, and if it doesn't, the method doesn't save
+        assertFalse(file.exists());
+
+
+    }
+
+
+    @Test
+    void sortByName() {
 
         //Arrange
 
@@ -169,7 +203,7 @@ class MovieCollectionTest implements fileHandlerInteface{
     }
 
     @Test
-    void sortByName(){
+    void sortByAttributes() {
 
         //Arrange
 
@@ -183,21 +217,7 @@ class MovieCollectionTest implements fileHandlerInteface{
     }
 
     @Test
-    void sortByAttributes(){
-
-        //Arrange
-
-
-        //Act
-
-
-        //Assert
-
-
-    }
-
-    @Test
-    void sortByMultiplyAttributes(){
+    void sortByMultiplyAttributes() {
 
         //Arrange
 
@@ -211,6 +231,9 @@ class MovieCollectionTest implements fileHandlerInteface{
     }
 
 
+
+    //Note: we overrided our filehandler interface methods so we were able to save to another file ("test.csv").
+    //Otherwise, it would save to our real "movieCollection.csv" file.
     @Override
     public void loadMovieCollection(MovieCollection movieCollection) {
         movieCollection.getMovieCollection().addAll(readCsvFile());
@@ -264,12 +287,20 @@ class MovieCollectionTest implements fileHandlerInteface{
         return loadedMovie;
     }
 
+    //Helper method to test load method
+    void saveMoviesToCsvFileHelper() {
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println("Title,Director,Year,IsInColor,LengthinMin,Genre");
+            printWriter.println("Die Hard,John McTiernan,1988,true,132,Action");
+            printWriter.println("The Shawshank Redemption,Frank Darabont,1994,true,142,Drama");
+            printWriter.println("Inception,Christopher Nolan,2010,true,148,Sci-Fi");
+            printWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
-
-
-
-
-
-
+    }
 }
